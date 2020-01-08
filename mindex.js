4f55ec562309b601cc6c9daded32fb62a60bdd79
allys-m <51463868+allys-m@users.noreply.github.com>
2020-01-08 23:58:33 +0100

    Update mindex.js

--- mindex.js
+++ mindex.js
@@ -6,6 +6,11 @@ const express = require('express');
 let m_main = class {
     m_app(){
       var app = express();
-      
+        
+          app.get('/', (req, res)=>{
+              res.send("<h1>TEST</h1>");
+              res.end();
+             
+          });
     }
 };
